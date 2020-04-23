1a995230e5da0120850e73273c0980aef35cf5c2
Luc de wit <luc.dewit@hotmail.nl>
2020-04-23 23:01:59 +0200

    end of day

--- neutronic.js
+++ neutronic.js
@@ -3,26 +3,14 @@ const isType = (value) => value === "string" || value === "number";
 const isName = (name) => !/^[0-9]/gm.test(name) && !name.includes(" ");
 const isString = (string) => string[0] === '\"' && string[string.length - 1] === '\"';
 const isNum = (string) => Number(string).toString() === string;
-const getType = (line) => {
-    const word = line.split(" ")[0];
-    return isType(word) ? word : "unkown";
-};
-const getName = (line) => {
-    let name = line.split("=")[0].split(" ");
-    name.shift();
-    name = name.join(" ").trim();
-    return name;
-};
-const getValue = (line, type) => {
-    if (type === "string") {
-        console.log(line.split("=")[1].trim());
-    }
-    else {
-        console.log(line.split("=")[1].trim());
-    }
-    return 0;
+const getName = (line) => line.split("=")[0].trim();
+const getValue = (line) => {
+    let value = line.split("=")[1].trim();
+    return isString(value) ? value.substring(1, value.length - 1) :
+        isNum(value) ? Number(value) : undefined;
 };
 const parseVars = (vars) => {
+    let varsFound = [];
     for (const varTag of vars) {
         let content = varTag.innerText.split(";");
         varTag.remove();
@@ -30,37 +18,100 @@ const parseVars = (vars) => {
             variable = variable.trim();
             if (variable !== "") {
                 let newVar = { type: "unkown", name: "", value: "" };
+                // syntax check
                 if (!variable.includes("=")) {
-                    console.log(`[ERROR] variable does not have "="\n${variable}`);
-                }
-                // get type
-                newVar.type = getType(variable);
-                // validate type
-                if (newVar.type === "unkown") {
-                    console.log(`[ERROR] unknown type\n${variable}`);
-                    continue;
+                    console.error(`[NEUTRONIC ERROR] Variable does not have "="\n${variable}`);
                 }
                 // get name
                 newVar.name = getName(variable);
                 // validate name
                 if (!isName(newVar.name)) {
-                    console.log(`[ERROR] illegal variable name "${newVar.name}"\n${variable}`);
+                    console.error(`[NEUTRONIC ERROR] Illegal variable name "${newVar.name}"\n${variable}`);
                     continue;
                 }
-                //get value
-                newVar.value = getValue(variable, newVar.type);
-                state.vars.push(newVar);
+                // get and validate value
+                let val = getValue(variable);
+                if (val === undefined) {
+                    console.error(`[NEUTRONIC ERROR] cannot infer value \n${variable}`);
+                    continue;
+                }
+                newVar.value = val;
+                // get type
+                typeof newVar.value == "string" ? newVar.type = "string" : newVar.type = "number";
+                varsFound.push(newVar);
             }
         }
-        //console.log(content);
+    }
+    return varsFound;
+};
+const identifyMustaches = () => {
+    let content = document.body.innerHTML;
+    let inMustache = false;
+    let varname = "";
+    let indexFound = 0;
+    for (let char = 0; char < content.length - 1; char++) {
+        if (!inMustache && content[char] === '{' && content[char + 1] === '{') {
+            char++;
+            indexFound = char;
+            inMustache = true;
+        }
+        else if (inMustache && content[char] === '}' && content[char + 1] === '}') {
+            inMustache = false;
+            content = content.slice(0, indexFound - 1) + `<span class="neutronic-variable neutronic-bind-${varname.trim()}"></span>` + content.slice(indexFound + varname.length + 3);
+            varname = "";
+        }
+        else if (inMustache) {
+            varname += content[char];
+        }
+    }
+    document.body.innerHTML = content;
+};
+const updateAllMustaches = () => {
+    let mustaches = document.getElementsByClassName("neutronic-variable");
+    for (const mustache of mustaches) {
+        let className = Array.from(mustache.classList).find(value => /neutronic-bind-/gm.test(value));
+        if (className === undefined || className === "neutronic-bind-") {
+            continue;
+        }
+        else {
+            className = className.substring(15);
+            let variable = state.vars.find(value => value.name === className);
+            if (variable !== undefined) {
+                mustache.innerHTML = variable.value;
+            }
+        }
+    }
+};
+const retrieveData = async (url) => {
+    let response = await fetch(url);
+    if (response.status !== 200) {
+        return undefined;
+    }
+    return await response.text();
+};
+const loadImports = async () => {
+    const importTags = document.getElementsByTagName("import");
+    for (const importTag of importTags) {
+        let pathAttr = Array.from(importTag.attributes).find(attr => attr.name == "src");
+        if (pathAttr === undefined) {
+            console.error(`[NEUTRONIC ERROR] import tag without src`);
+            continue;
+        }
+        let data = await retrieveData(`http://localhost:5000/testing/${pathAttr.value}`);
+        if (data === undefined) {
+            console.error(`[NEUTRONIC ERROR] cannot find or open import: "${pathAttr.value}"`);
+            continue;
+        }
+        importTag.innerHTML = data;
+        console.log(importTag.innerHTML);
     }
 };
 let state = {
-    vars: []
+    vars: [],
 };
-window.onload = () => {
-    parseVars(document.getElementsByTagName("vars"));
-    for (const variable of state.vars) {
-        console.log(variable);
-    }
+window.onload = async () => {
+    await loadImports();
+    state.vars = parseVars(document.getElementsByTagName("vars"));
+    identifyMustaches();
+    updateAllMustaches();
 };
